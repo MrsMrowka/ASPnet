16b23f71241f0b458ba715b112cb1e280d50c60d
Mrowka333 <39970211+Mrowka333@users.noreply.github.com>
2020-10-08 16:15:04 +0200

    secondExercise

---
+++ mvc/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShopList()
+        {
+            var list = new List<ShopModel>
+            {
+                new ShopModel { ShopItems = "T-shirt" },
+                new ShopModel { ShopItems = "Shoes" },
+                new ShopModel { ShopItems = "Cardigan" },
+                new ShopModel { ShopItems = "Leather Belt" },
+            };
+
+            return View(list);
+        }
+    }
+}
---
+++ mvc/Models/ShopModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class ShopModel
+    {
+        public string ShopItems { get; set; }
+    }
+}
