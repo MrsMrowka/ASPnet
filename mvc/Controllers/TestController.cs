1a67702801d44d076e165586c49e0bacb6e0887f
Mrowka333 <39970211+Mrowka333@users.noreply.github.com>
2020-10-08 15:22:15 +0200

    firstExercise

---
+++ mvc/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string StringValue()
+        {
+            return "Hello world!";
+        }
+
+        public IActionResult GoogleRedirect()
+        {
+            return Redirect("https://www.google.com/");
+        }
+
+        public IActionResult GetJson()
+        {
+            return Json(new {Name = "Jan", Surname = "Kowalski" });
+        }
+    }
+}
